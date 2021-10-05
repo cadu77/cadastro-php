3c9d8ac4ad9aa4fa92979a5a77e7dc9ee92e2367
Eduardo Ferreira de Jesus <eduardo-1997@outlook.com.br>
2021-10-04 21:05:44 -0300

    configuracao-data-base-php

--- cadastro/config.php
+++ cadastro/config.php
@@ -1,8 +1,18 @@
 <?php
-  ?dbHost = 'Localhost';
-  ?dbUsername = 'root';
-  ?dbPassword = '';
-  ?dbName = 'formulario-php'
 
-  ?conexao = new mysqli(?dbHost,?dbUsername,?dbPassword,?dbName);
+  $dbHost = 'Localhost';
+  $dbUsername = 'root';
+  $dbPassword = '';
+  $dbName = 'formulario-php';
+
+  $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+  //if($conexao->connect_errno){
+  //  echo "erro";
+  //}
+  //else{
+  //  echo "certo";
+  // }
+
+  
 ?>
